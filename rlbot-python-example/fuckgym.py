f7914dd65476c21847fcc9875e2c3b82c41e9cad
songjoon <songjoon000@gmail.com>
2025-03-31 01:37:50 +0900

    Solved issue that performance drops whenever we reset. - Debugging is awful

--- rlbot-python-example/fuckgym.py
+++ rlbot-python-example/fuckgym.py
@@ -4,7 +4,7 @@ from rlbot.managers import MatchManager
 from pathlib import Path
 from time import sleep
 from typing import Optional
-
+import math
 import os
 from traceback import print_exc
 from typing import Optional
@@ -18,6 +18,7 @@ from rlbot.interface import (
 )
 from rlbot.flat import BallAnchor, ControllerState, GamePacket
 from rlbot.managers import Bot, Hivemind
+from rlbot.flat import DesiredBallState, DesiredCarState, DesiredMatchInfo, DesiredPhysics, RotatorPartial, Vector3Partial
 
 MATCH_CONFIG_PATH = "rlbot.toml"
 RLBOT_SERVER_IP = "127.0.0.1"
@@ -30,7 +31,7 @@ class Gym:
         # ONLY start the match
         self.match_manager = MatchManager()
         self.match_manager.start_match(self.root_dir / MATCH_CONFIG_PATH)
-                
+        self.connect()
         
         
     def connect(self):
@@ -38,7 +39,9 @@ class Gym:
         rlbot_server_port = int(os.environ.get("RLBOT_SERVER_PORT", RLBOT_SERVER_PORT))
         
         self.bot1 = Hivemind("gym1")
+        self.bot1.team = 0
         self.bot2 = Hivemind("gym2")
+        self.bot2.team = 1
         try:
             self.bot1._game_interface.connect(
                 wants_match_communications=True,
@@ -46,15 +49,15 @@ class Gym:
                 rlbot_server_ip=rlbot_server_ip,
                 rlbot_server_port=rlbot_server_port,
             )
-            
+            sleep(5)
             self.bot2._game_interface.connect(
                 wants_match_communications=True,
                 wants_ball_predictions=True ,
                 rlbot_server_ip=rlbot_server_ip,
                 rlbot_server_port=rlbot_server_port,
             )
-            self.bot1._try_initialize() # IDK if this is needed
-            self.bot2._try_initialize()
+            #print("FUCKLFUCKFUCKFUCKFUCKFUCKFUCKFUCK")
+            
         except Exception as e:
             self._logger.error("Unexpected error: %s", e)
     def step(self, action : tuple[dict[int, ControllerState]]):
@@ -90,12 +93,63 @@ class Gym:
             )
         return (self.bot1._latest_packet, self.bot1._latest_prediction), (self.bot2._latest_packet, self.bot2._latest_prediction)
     def reset(self):
-        
-        self.match_manager.shut_down()
-        self.match_manager = MatchManager()
-        self.match_manager.start_match(self.root_dir / MATCH_CONFIG_PATH)
-        
-        self.connect()
+        car_state = {
+            0: DesiredCarState(
+            physics=DesiredPhysics(
+                location=Vector3Partial(x=-2048.0, y=-2560.0, z=35.32999801635742),
+                rotation=RotatorPartial(pitch=0.0, yaw=0.7853981852531433, roll=0.0),
+                velocity=Vector3Partial(x=0.0, y=0.0, z=-27.05099868774414),
+                angular_velocity=Vector3Partial(x=0.0, y=0.0, z=0.0)
+            )
+            ),
+            1: DesiredCarState(
+            physics=DesiredPhysics(
+                location=Vector3Partial(x=0.0, y=-4608.0, z=35.32999801635742),
+                rotation=RotatorPartial(pitch=0.0, yaw=1.5707963705062866, roll=0.0),
+                velocity=Vector3Partial(x=0.0, y=0.0, z=-27.05099868774414),
+                angular_velocity=Vector3Partial(x=0.0, y=0.0, z=0.0)
+            )
+            ),
+            2: DesiredCarState(
+            physics=DesiredPhysics(
+                location=Vector3Partial(x=-256.0, y=-3840.0, z=35.32999801635742),
+                rotation=RotatorPartial(pitch=0.0, yaw=1.5707963705062866, roll=0.0),
+                velocity=Vector3Partial(x=0.0, y=0.0, z=-27.05099868774414),
+                angular_velocity=Vector3Partial(x=0.0, y=0.0, z=0.0)
+            )
+            ),
+            3: DesiredCarState(
+            physics=DesiredPhysics(
+                location=Vector3Partial(x=2048.0, y=2560.0, z=35.32999801635742),
+                rotation=RotatorPartial(pitch=0.0, yaw=-2.356194496154785, roll=0.0),
+                velocity=Vector3Partial(x=0.0, y=0.0, z=-27.05099868774414),
+                angular_velocity=Vector3Partial(x=0.0, y=0.0, z=0.0)
+            )
+            ),
+            4: DesiredCarState(
+            physics=DesiredPhysics(
+                location=Vector3Partial(x=0.0, y=4608.0, z=35.32999801635742),
+                rotation=RotatorPartial(pitch=0.0, yaw=-1.5707963705062866, roll=0.0),
+                velocity=Vector3Partial(x=0.0, y=0.0, z=-27.05099868774414),
+                angular_velocity=Vector3Partial(x=0.0, y=0.0, z=0.0)
+            )
+            ),
+            5: DesiredCarState(
+            physics=DesiredPhysics(
+                location=Vector3Partial(x=256.0, y=3840.0, z=35.32999801635742),
+                rotation=RotatorPartial(pitch=0.0, yaw=-1.5707963705062866, roll=0.0),
+                velocity=Vector3Partial(x=0.0, y=0.0, z=-27.05099868774414),
+                angular_velocity=Vector3Partial(x=0.0, y=0.0, z=0.0)
+            )
+            ),
+        }
+
+        ball_state = DesiredBallState(DesiredPhysics(location=Vector3Partial(0, 0, z=35.32999801635742),
+                                                     velocity=Vector3Partial(0, 0, 0),
+                                                     angular_velocity=Vector3Partial(0, 0, 0),
+                                                        rotation=RotatorPartial(0, 0, 0)))
+        self.match_manager.set_game_state(balls={0: ball_state}, cars=car_state)
+        #self.connect()
         while(True):
             self.bot1._game_interface.handle_incoming_messages(
                     blocking=True
@@ -108,7 +162,7 @@ class Gym:
             if(len(self.bot1._latest_packet.players) == 6 and len(self.bot2._latest_packet.players) == 6):
                 break
         
-        return (self.bot1._latest_packet, self.bot1._latest_prediction), (self.bot2._latest_packet, self.bot2._latest_prediction), {"field_info" : self.bot1.field_info, "indices1" : self.bot1.indices, "indices2" : self.bot1.indices}
+        return (self.bot1._latest_packet, self.bot1._latest_prediction), (self.bot2._latest_packet, self.bot2._latest_prediction), {"field_info1" : self.bot1.field_info,"field_info2" : self.bot2.field_info, "indices1" : self.bot1.indices, "indices2" : self.bot2.indices}
     def close(self):
         self.match_manager.stop_match()
         self.match_manager.disconnect()
@@ -119,10 +173,9 @@ if __name__ == "__main__":
     
     ob1,ob2, info = gym.reset()
     
-    #print(info)
     myhive, ophive = MyHive(), MyHive()
-    myhive.initialize(field_info = info["field_info"], indices = info["indices1"])
-    ophive.initialize(field_info = info["field_info"], indices = info["indices2"])
+    myhive.initialize(field_info = info["field_info1"], indices = info["indices1"])
+    ophive.initialize(field_info = info["field_info2"], indices = info["indices2"])
     i = 0
     while(True):
         try:
@@ -130,7 +183,6 @@ if __name__ == "__main__":
             action2 = ophive.get_outputs(*ob2)
             
             actions = (action1, action2)
-            #print(actions)
             ob1,ob2 = gym.step(actions)
         except Exception as e:
             print("Unexpected error: %s" % e)
